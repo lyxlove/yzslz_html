765e1d7d34a5022aa34334540b314efe01bb4968
yyx <443342311@qq.com>
2018-05-22 14:35:07 +0800

    update

--- y_js/log_yzlsz.js
+++ y_js/log_yzlsz.js
@@ -38,3 +38,42 @@ function ShowHideDiv() {
       })
 
 }
+
+
+function TestRe() {
+    //var strValue = $('#reText').val();
+    //$('#tTable').append("<tr><td>"+strValue+"</td></tr>");
+    alert(/w/.test("rw"));
+}
+
+
+
+/*是否带有小数*/
+function    isDecimal(strValue )  {
+   var  objRegExp= /^\d+\.\d+$/;
+   return  objRegExp.test(strValue);
+}
+
+/*校验是否中文名称组成 */
+function ischina(str) {
+    var reg=/^[\u4E00-\u9FA5]{2,4}$/;   /*定义验证表达式*/
+    return reg.test(str);     /*进行验证*/
+}
+
+/*校验是否全由8位数字组成 */
+function isStudentNo(str) {
+    var reg=/^[0-9]{8}$/;   /*定义验证表达式*/
+    return reg.test(str);     /*进行验证*/
+}
+
+/*校验电话码格式 */
+function isTelCode(str) {
+    var reg= /^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/;
+    return reg.test(str);
+}
+
+/*校验邮件地址是否合法 */
+function IsEmail(str) {
+    var reg=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+$/;
+    return reg.test(str);
+}
